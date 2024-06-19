4849fe1b866f5f9c80ac92dd4fd24c5a5808c3d3
Hector Merejo <Hector.Merejo@mail.citytech.cuny.edu>
2024-06-19 19:09:39 -0400

    added photo reel

---
+++ photoReel.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide) => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  setInterval(nextSlide, 3000); // Change slide every 3 seconds
+});
