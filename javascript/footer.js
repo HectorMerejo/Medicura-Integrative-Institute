75ba732837e0dadb9f10eea5d3c8d7d57f53d78b
Hector Merejo <HMerejojr@gmail.com>
2025-03-26 06:01:56 -0400

    made links relative paths

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,6 +1,6 @@
 // footer.js
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/html/footer.html")
+  fetch("html/footer.html")
       .then((response) => response.text())
       .then((data) => {
           document.getElementById("footer-placeholder").innerHTML = data;
